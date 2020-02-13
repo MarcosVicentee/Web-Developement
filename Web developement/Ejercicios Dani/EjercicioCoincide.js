9e57264241613bfef03e455573d3c70b8e121117
marcosvicentee <marcos.vicente.diez@gmail.com>
2020-02-13 13:59:17 +0100

    EjercicioCoincide Completed

--- Web developement/Ejercicios Dani/EjercicioCoincide.js
+++ Web developement/Ejercicios Dani/EjercicioCoincide.js
@@ -1,9 +1,12 @@
 
 
 function coincide(arr){
-    return arr.filter(num => indexOf())
+    return arr.filter((num, index) => {
+        if (num === index) 
+            return index })
 }
 
 
 
-console.log(coincide([1,3,2,5,2,5,6]))
+console.log(coincide([1,3,2,5,2,5,6,7]))
+
