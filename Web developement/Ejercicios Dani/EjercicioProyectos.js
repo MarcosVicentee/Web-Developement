8b66d4690ee2d46e94f6ef00e08a8b16bbb948ed
marcosvicentee <marcos.vicente.diez@gmail.com>
2020-02-20 14:00:39 +0100

    Sustancial changes in EjercicioProyectos

--- Web developement/Ejercicios Dani/EjercicioProyectos.js
+++ Web developement/Ejercicios Dani/EjercicioProyectos.js
@@ -3,59 +3,55 @@
 function mejoresPresupuestos(proyectosOfrecidos, presupuestosOfrecidos){
     var arrPresupuestos = [];
     var arrayIndices = [];
-    var arrayValores = [];
+    
     total = 0;
 
     if (contieneNumeros(proyectosOfrecidos, n)){
-        proyectosOfrecidos.forEach(element => { //Por cada elemento en pRoyectosOfrecidos
-            {
-                arrayIndices = cogerIndices(proyectosOfrecidos, element);
-                arrayValores = cogerValores(arrayIndices, element);
+        proyectosOfrecidos.forEach(element => { //Por cada elemento en ProyectosOfrecidos
+            var arrayValores = [];
+            arrayIndices = cogerIndices(proyectosOfrecidos, element);
+            console.log("ArrayIndices [ " + arrayIndices + " ]")
+            
 
-                arrayIndices.forEach(indice => { //Por cada elemento de indices. ESTO SE QUITARIA SI FUNCIONA LO DE COGERVALORES [10, 30]
-                     valueOf(arrayIndices[indice]) = presupuestosOfrecidos[indice];
-                    console.log(arrayIndices)
-                    
-                });
-                arrPresupuestos[indice] = Math.min.apply(Math, array); //Meto el minimo valor de los presupuestos ofrecidos.
-            }
+
+            arrayIndices.forEach(valor => { //Por cada elemento de indices.
+                arrayValores.push(presupuestosOfrecidos[valor])  //Metemos en el arrayValores los elementos de presupuestosOfrecidos en las posiciones del arrayIndice
+
+            });
+
+
+            console.log("Array Valores [ " + arrayValores + " ]")
+            arrPresupuestos.push(Math.min.apply(Math, arrayValores)); //Meto el minimo valor de los presupuestos ofrecidos.
+            console.log("Array Presupuestos [ " + arrPresupuestos + " ]")
+        
+
+            //LO SIGUIENTE ES QUE NO EVELUE EL PROYECTO 2 VECES SI YA SE HA EVALUADO => ARRAY DE PROYECTOS EVALUADOS O METODO QUE DEVOLVIA EL PRIMER ELEMENTO QUE COINDIDIA CON EL VALOR O ALGO ASI  
+            
         });
         return arrPresupuestos;
     }
     else
         return -1;   
-
 }
 
 
 function contieneNumeros(arr, n){
-    let cont = 1;
-    for (let i=0; i<5; i++){
-        if (arr.includes(i))
-            cont++;
-            console.log(cont)
-    }
-    if (cont==n) return true;
+    var unique = arr.filter((value, index, arr) => arr.indexOf(value) === index); //https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+    
+    if (unique.length == n) return true;
     else return false;
 }
 
-function cogerIndices(arr, val) {
+
+function cogerIndices(arr, val) {   // Usar map()
     var indexes = [];
     for(let i = 0; i < arr.length; i++)
         if (arr[i] === val)
             indexes.push(i);
-    console.log(indexes);
     
     return indexes;
 }
 
-function cogerValores(arr, indice){
-    var valores = [];
-    for(let i = 0; i < arr.length; i++)
-        if (valueOf(arr[i]) === indice)
-            valores.push(i);
-    console.log(valores)
-}
 
 
 
